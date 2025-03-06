11e3cd16f15d16b6e28cca4dd94e14dbfc39ea20
makstell_ <naivemax@gmail.com>
2025-03-06 11:29:05 +0200

    Update and rename 100 lat.py to 100 lat_rev01.py
    correct exclamation marks

---
+++ 100 lat_rev01.py
@@ -0,0 +1,19 @@
+import datetime
+
+name = input("Cześć, jak masz na imię!")
+age = int(input("Ile masz lat? " + name + "!"))
+yeartoday = datetime.datetime.now().year
+if 0 <= age <= 99:
+    data_actual = yeartoday + (100 - age)
+    print(
+        "Wiesz, że w roku "
+        + str(data_actual)
+        + " będziemy obchodzić Twoje setne "
+        + "(100lat!) urodziny. "
+    )
+elif age < 0:
+    print("Fajny żart, nie ma takich lat!")
+elif 100 <= age <= 199:
+    print("Gratulacje, przekroczyłeś pierwszą setkę!")
+else:
+    print("Kurcza, naprawde estesz Dunkanem Maklaudem))")
